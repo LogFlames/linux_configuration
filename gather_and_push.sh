e6a39a9f9bb268980d49ecca236c40f451469714
Elias Lundell <elilun03@gmail.com>
2021-11-06 17:14:31 +0100

    Prompt for commit message and ask wether to push after showing status

--- gather_and_push.sh
+++ gather_and_push.sh
@@ -6,6 +6,13 @@ rsync -a ~/.config/kitty ./ -r
 rsync -a ~/.config/kak ./ -r
 rsync -a ~/.config/htop ./ -r
 
-git add .
-git commit -m "Automatic push"
-git push
+git status
+
+read -p "Do you wish to push this to github? Enter the commit message, to NOT push press enter: " commit_msg
+if [[ ! $commit_msg = "" ]]; then
+    git add .
+    git commit -m "$commit_msg"
+    git push
+else
+    echo "Nothing has been commited nor pushed"
+fi
