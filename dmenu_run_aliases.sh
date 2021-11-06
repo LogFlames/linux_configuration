bc3c6206eab41a00f61f4ba95592b97708a60703
Elias Lundell <elilun03@gmail.com>
2021-11-06 17:15:48 +0100

    Using cache

--- dmenu_run_aliases.sh
+++ dmenu_run_aliases.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+cachedir=${XDG_CACHE_HOME:-"$HOME/.cache"}
+if [ -d "$cachedir" ]; then
+    cache=$cachedir/dmenu_run
+else
+    cache=$HOME/.dmenu_cache
+fi
+
 if [ -f ~/.bash_aliases ]; then
     aliases=( ~/.bash_aliases )
 fi
@@ -10,14 +17,18 @@ fi
 
 source $aliases
 
-cmd=$(
-    IFS=:
-    (
+cmd=`(
+IFS=:
+    if stest -dqr -n "$cache" $PATH || stest -fqr -n "$cache" "$aliases" || stest -fqr -n "$cache" "$functions"; then
+        (
         stest -flx $PATH
         alias | awk -F '[ =]' '{print $2}'
         compgen -A function
-    ) | sort -u | dmenu "$@"
-   )
+        ) | sort -u | tee "$cache" | dmenu "$@"
+    else
+        dmenu "$@" < "$cache"
+    fi
+   )`
 
 if [ -f ~/.bash_aliases ]; then
     if [ ! -z "$(grep '^alias' $aliases|cut -d'=' -f1|grep $cmd)" ] || [ -z $(which $cmd) ]; then
