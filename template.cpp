61ca3f9ae8c4bcb0f2a043229561b128bc9ffd67
Elias Lundell <elilun03@gmail.com>
2021-11-23 21:07:38 +0100

    Add paren to ensure nothing goes wrong with the defines

--- template.cpp
+++ template.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 #define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define per(i, a, b) for (int i = a - 1; i >= (b); --i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
+#define trav(a, x) for(auto& a : (x))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
-#define deb(x) cout << #x << " = " << x << endl;
-#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << endl;
-#define debv(a) cout << #a << ": [ "; rep(i,0,sz(a)) { cout << a[i] << " "; } cout << "]" << endl;
+#define deb(x) cout << #x << " = " << (x) << endl;
+#define deb2(x, y) cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define debv(a) cout << #a << ": [ "; trav(i,(a)) { cout << i << " "; } cout << "]" << endl;
 #define gtz(a, b) ((a) > 0 ? (b) : 0)
 #define pb push_back
 #define un_map unordered_map
