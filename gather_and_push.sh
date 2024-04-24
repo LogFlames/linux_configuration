39fae65bb8dfbe6cd5db05ed35141f3a0d0cb0c3
Elias Lundell <elilun03@gmail.com>
2024-04-24 16:26:14 +0200

    update kitty to more scroll-lines, add sway and tofi config

--- gather_and_push.sh
+++ gather_and_push.sh
@@ -9,6 +9,8 @@ rsync -a ~/.config/awesome ./ -r
 rsync -a ~/.config/kitty ./ -r
 rsync -a ~/.config/kak ./ -r
 rsync -a ~/.config/htop ./ -r
+rsync -a ~/.config/sway ./ -r
+rsync -a ~/.config/tofi ./ -r
 rsync -a ~/.doom.d/* ./doom.d -r
 rsync -a ~/.xbindkeysrc ./
 rsync -a ~/.aliases ./aliases -r
