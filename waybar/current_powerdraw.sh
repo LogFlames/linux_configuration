c388d60afae107543ea2eb7d83cf35628aec2b50
Elias Lundell <elilun03@gmail.com>
2024-05-06 12:54:34 +0200

    reduce to one decimal

--- waybar/current_powerdraw.sh
+++ waybar/current_powerdraw.sh
@@ -1,1 +1,1 @@
-cat /sys/class/power_supply/BAT0/{current_now,voltage_now} | tr '\n' ' ' | awk '{printf "%.2f W", $1*10^-6 * $2 * 10^-6}'
+cat /sys/class/power_supply/BAT0/{current_now,voltage_now} | tr '\n' ' ' | awk '{printf "%.1f W", $1*10^-6 * $2 * 10^-6}'
